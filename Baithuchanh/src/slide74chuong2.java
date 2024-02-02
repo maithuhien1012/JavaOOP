cdbb1fdc62d1fd5c2f62b3ae6aaa209c43755ac7
maithuhien1012 <mhien101204@gmail.com>
2024-02-02 10:24:05 +0700

    slide81chuong2

--- Baithuchanh/src/slide74chuong2.java
+++ Baithuchanh/src/slide74chuong2.java
@@ -10,7 +10,7 @@ public class slide74chuong2 {
         int A[] = new int[n];
         System.out.println("Nhap cac phan tu cho mang: ");
         for (int i = 0; i < n; i++){
-            System.out.print("Nhap phan tu thu "+i +": ");
+            System.out.println("Nhap phan tu thu "+i +": ");
             A[i] = sc.nextInt();
         }
         double Tong = 0;
