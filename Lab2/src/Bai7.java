18baf06eeec680fcce45536da62388594fcf237a
maithuhien1012 <mhien101204@gmail.com>
2024-02-08 16:39:24 +0700

    bai8

--- Lab2/src/Bai7.java
+++ Lab2/src/Bai7.java
@@ -1,6 +1,6 @@
 public class Bai7 {
     public static void main(String[] args) {
-        for(int i = 1;i<=20;i++) {
+        for(int i = 1; i <= 20; i++) {
             System.out.print(i + " ");
         }
     }
